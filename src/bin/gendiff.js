5f1126dcc954c72c95e9edbd86c24e2b76516915
Olga Melnikova <shelga.melnikova@gmail.com>
2020-03-09 16:27:01 +0000

    first commit

---
+++ src/bin/gendiff.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+import commander from 'commander';
+
+const program = new commander.Command();
+
+program
+    .version('0.1.0')
+    .description('Compares two configuration files and shows a difference.');
+    console.log("hello");
+    
+program.parse(process.argv);
